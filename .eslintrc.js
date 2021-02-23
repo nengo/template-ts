9d7884554dd83f0862b23b0d669b02b8cbc0d76f
Trevor Bekolay <tbekolay@gmail.com>
2021-02-23 09:35:19 -0600

    Simplify and make more generic
    - Ran prettier on all files
    - Removed comments from .eslintrc.js
    - Removed comments from .gitignore and sorted lines alphbetically
    - Minor style changes to README.md
    - Changed some things to TODO in package.json
    - Removed pre-specified dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,37 @@
 module.exports = {
-    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-    parserOptions: {
-      ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-      sourceType: 'module', // Allows for the use of imports
-      ecmaFeatures: {
-        jsx: true, // Allows for the parsing of JSX
-      },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    settings: {
-      react: {
-        version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
-      },
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    extends: [
-      'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-      'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-      'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended', // Must be last in the extends array
+  ],
+  plugins: ['sort-imports-es6-autofix'],
+  rules: {
+    'sort-imports-es6-autofix/sort-imports-es6': [
+      2,
+      {
+        ignoreCase: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      },
     ],
-  
-    plugins: ['sort-imports-es6-autofix'],
-    rules: {
-      'sort-imports-es6-autofix/sort-imports-es6': [
-        2,
-        {
-          ignoreCase: false,
-          ignoreMemberSort: false,
-          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-        },
-      ],
-      // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-      // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-      'prettier/prettier': [
-        'error',
-        {
-          endOfLine: 'auto',
-        },
-      ],
-    },
-  };
-  
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
--- README.md
+++ README.md
@@ -1,23 +1,29 @@
-# Typescript GUI Template
-This is a template repository for new projects. When you create a new repository from this template, follow these instructions:
+# Typescript project template
 
-1. Make sure the `.gitignore` file is appropriate for your project
-2. Update package.json properties to describe the package:
-   - `name`
-   - `version`
-   - `description`
-3. Setup `@nengo/commitlint-config-nengo`
-   - Instructions [here](https://github.com/nengo/commitlint-config-nengo#setup)
-4. Install dependecies with `npm install`
-5. Setup testing:
-   1. Remove the `--passWithNoTests` flag from `test-coverage` script in `package.json`
-   2. The project defaults to checking for tests in the `/utils` folder. You can modify the folders to check for tests in `package.json`
-   ```json
-   "jest":{
-      "collectCoverageFrom": [
-         "<rootDir>/src/utils/**/*.ts",
-         ...
-      ]
-   }
-   ```
+This is a template repository for new Typescript projects.
+When you create a new repository from this template, follow these instructions:
+
+1. Make sure the `.gitignore` file is appropriate for your project.
+
+2. Replace the TODO keys like `name` and `description` in `package.json`.
+
+3. [Set up `@nengo/commitlint-config-nengo`.](https://github.com/nengo/commitlint-config-nengo#setup)
+
+4. Install dependecies with `npm install`.
+
+5. Set up testing in `package.json`:
+
+   1. Remove the `--passWithNoTests` flag from the `test-coverage` script.
+
+   2. Modify the test collection rules, if appropriate.
+
+      ```json
+      "jest": {
+         "collectCoverageFrom": [
+            "<rootDir>/src/utils/**/*.ts",
+            ...
+         ]
+      }
+      ```
+
 6. Replace the text in `README.md` with text for the new repository.
